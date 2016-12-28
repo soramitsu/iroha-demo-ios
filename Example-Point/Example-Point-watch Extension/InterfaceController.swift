64fbcd62168a088438f56ceaa59410283325b533
luca3104 <s1210006@u-aizu.ac.jp>
2016-12-28 11:28:03 +0900

    added watchOS target

---
+++ Example-Point/Example-Point-watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Example-Point-watch Extension
+//
+//  Created by Kaji Satoshi on 2016/12/28.
+//  Copyright © 2016年 Soramitsu Co., Ltd. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
